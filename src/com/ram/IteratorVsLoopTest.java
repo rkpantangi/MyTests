865c832a412cb9b759634c78c230cefcfc0d5c5f
rkpantangi <rkpantangi@gmail.com>
2014-01-19 16:26:45 -0500

    Iterator vs index loop performance test

---
+++ src/com/ram/IteratorVsLoopTest.java
@@ -0,0 +1,34 @@
+package com.ram;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorVsLoopTest {
+
+	public static void main(String[] args) {
+
+		List<String> strList = new ArrayList<>(100);
+		for (int i=1;i<=1000;i++) {
+			strList.add("abc");
+		}
+		
+		long t1 = System.nanoTime();
+		for (int i=0;i<strList.size();i++) {
+			@SuppressWarnings("unused")
+			String s = strList.get(i); 
+		}
+		long t2 = System.nanoTime() - t1;
+		System.out.println("Time using index loop (nanos): " + t2);
+
+		t1 = System.nanoTime();
+		Iterator<String> iter = strList.iterator();
+		while(iter.hasNext()) {
+			@SuppressWarnings("unused")
+			String s = iter.next();
+		}
+		t2 = System.nanoTime() - t1;
+		System.out.println("Time using Iterator (nanos): " + t2);
+	}
+
+}
